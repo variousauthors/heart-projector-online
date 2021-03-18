d5c5a0ca46dfdfafad87ec61f839a5c6385c9855
Brendan Vance <brendan.vance@gmail.com>
2021-03-18 03:07:37 -0700

    Worked on a cool lighting system for TPCA colour room

--- data.js
+++ data.js
@@ -181,10 +181,10 @@ module.exports.ROOMS = {
             ff0000: { cmd: "enter", room: "TPCAHallway", label: "out", point: [65, 94], enterPoint: [32, 16], obstacle: false },
         },
 		things: {
-			instrument: {file: "colourRoom-Music.png", position: [96, 15], obstacle: true, lightEmissionColour: "#FF0000" },
-			voice: {file: "colourRoom-Voice.png", position: [20, 73], frames: 3, frameDelay: 11, obstacle: true, lightEmissionColour: "#FF00FF"},
-			blender: {file: "colourRoom-Blender.png", position: [95, 58], obstacle: true, lightEmissionColour: "#0000FF"},
-			Footstep: {file: "colourRoom-Footstep.png", position: [21, 21], frames: 3, frameDelay: 10, obstacle: true, lightEmissionColour: "#00ff00" },
+			instrument: {file: "colourRoom-Music.png", position: [96, 15], obstacle: true, lightEmissionColour: "#e8d42e" },
+			voice: {file: "colourRoom-Voice.png", position: [20, 73], frames: 3, frameDelay: 11, obstacle: true, lightEmissionColour: "#e82e3a"},
+			blender: {file: "colourRoom-Blender.png", position: [95, 58], obstacle: true, lightEmissionColour: "#73dff4"},
+			Footstep: {file: "colourRoom-Footstep.png", position: [21, 21], frames: 3, frameDelay: 10, obstacle: true, lightEmissionColour: "#ac73f4" },
 		},
     },
 	
