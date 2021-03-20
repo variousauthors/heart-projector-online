81665d94b564f318afaebc1b6f7aeda02b2e1300
negaspace <69841663+negaspace@users.noreply.github.com>
2021-03-20 02:13:43 -0700

    Fixed some entry/exit points
    Fixed some entry/exit points after changing TPCAHallway layout a bit, also fixed some image positions in TPCAcheckin

--- data.js
+++ data.js
@@ -106,13 +106,13 @@ module.exports.ROOMS = {
         area: "TPCAhallway-areas.png",
         areaColors: {
             // exits
-            hff0000: { cmd: "enter", room: "TPCAcheckin", label: "Check-Up Room", point: [33, 13], enterPoint: [38, 62], obstacle: false },
-			hff0000: { cmd: "enter", room: "TPCAcheckin", label: "Go to work", point: [115, 82], enterPoint: [86, 67], obstacle: false },
-            h00ff00: { cmd: "enter", room: "TPCAMapRoom", label: "Map Room", point: [121, 52], obstacle: false },
-            hf000ff: { cmd: "enter", room: "TPCATavern", label: "Tavern", point: [6, 51], enterPoint: [3, 50], obstacle: false },
+            h7c7c7c: { cmd: "enter", room: "TPCAcheckin", label: "Check-Up Room", point: [8, 30], enterPoint: [38, 62], obstacle: false },
+			h00ffee: { cmd: "enter", room: "TPCAcheckin", label: "Go to work", point: [120, 30], enterPoint: [86, 67], obstacle: false },
+            h00ff00: { cmd: "enter", room: "TPCAMapRoom", label: "Map Room", point: [121, 60], obstacle: false },
+            hf000ff: { cmd: "enter", room: "TPCATavern", label: "The Filthy Lyre", point: [8, 61], enterPoint: [3, 50], obstacle: false },
             h0000ff: { cmd: "enter", room: "TPCAFireRoom", label: "Bonfire", point: [96, 13], enterPoint: [15, 94], obstacle: false },
             h55ffff: { cmd: "enter", room: "elevator", label: "Elevator", point: [66, 97], enterPoint: [64, 68], obstacle: false },
-			h7c7c7c: { cmd: "enter", room: "TPCAColourRoom", label: "Lost and Found", point: [63, 7], enterPoint: [67, 94], obstacle: false },
+			hff0000: { cmd: "enter", room: "TPCAColourRoom", label: "Lost and Found", point: [32, 12], enterPoint: [67, 94], obstacle: false },
 			
 			//placard
             hfeff00: { cmd: "text", label: "The Papercut Arcade", txt: "The Papercut Arcade collective works to create safer spaces to explore creativity and art with like-minded folks. We're proud to be sharing digital real estate with the folks at Heart Projector.", point: [66, 73], align: "center", lines: 6, },
@@ -129,20 +129,20 @@ module.exports.ROOMS = {
         area: "TPCA-checkin-areas.png",
         areaColors: {
             // exits
-            h0000ff: { cmd: "enter", room: "TPCAHallway", label: "exit", point: [18, 74], enterPoint: [32, 16], obstacle: false },
-			hff0000: { cmd: "enter", room: "TPCAHallway", label: "exit", point: [94, 44], enterPoint: [115, 82], obstacle: false },
+            h0000ff: { cmd: "enter", room: "TPCAHallway", label: "exit", point: [18, 74], enterPoint: [12, 30], obstacle: false },
+			hff0000: { cmd: "enter", room: "TPCAHallway", label: "exit", point: [94, 44], enterPoint: [120, 30], obstacle: false },
 			hf000ff: { cmd: "text", label: "sense", txt: "Someone cares about you.", align: "center", lines: 1, point: [25, 58], obstacle: false },
 			hfff000: { cmd: "text", label: "marvel", txt: "That's unusual.", align: "center", lines: 1, point: [70, 45], obstacle: false },
-        	h00ff00: { cmd: "text", label: "spectate", txt: "This wasn't in the books", align: "center", lines: 1, point: [84, 45], obstacle: false },
+        	h00ff00: { cmd: "text", label: "spectate", txt: "This wasn't in the medical books!", align: "center", lines: 1, point: [84, 45], obstacle: false },
 			h7000ff: { cmd: "text", label: "wonder", txt: "A strong reaction?", align: "center", lines: 1, point: [83, 75], obstacle: false },
 			h00feff: { cmd: "text", label: "consider", txt: "More questions to ask.", align: "center", lines: 1, point: [86, 70], obstacle: false },
         	hffae00: { cmd: "text", label: "observe", txt: "A curious sample.", align: "center", lines: 1, point: [90, 80], obstacle: false },
 		},
 		things: {
 			mri: { file: "TPCA-checkin-mri.png", frames: 4, frameDelay: 5, position: [63, 17], depthAdjust:-2 },
-            skelly: { file: "TPCA-checkin-skelly.png", frames: 4, frameDelay: 20, position: [79, 13], depthAdjust: -2},
+            skelly: { file: "TPCA-checkin-skelly.png", frames: 4, frameDelay: 20, position: [79, 14], depthAdjust: -2},
             heart: { file: "TPCA-checkin-heart.png", frames: 2, frameDelay: 30, position: [103, 32]},
-			middle: { file: "TPCA-checkin-middle.png", position: [47, 13], depthAdjust: 50, obstacle: true},	
+			middle: { file: "TPCA-checkin-middle.png", position: [47, 11], depthAdjust: 50, obstacle: true},	
 		},
 	},
 
@@ -170,7 +170,7 @@ module.exports.ROOMS = {
         area: "maproom-areas.png",
 		areaColors: {
             // exits
-            hff0000: { cmd: "enter", room: "TPCAHallway", label: "Unceded territory", point: [10, 19], enterPoint: [117, 50], obstacle: false },
+            hff0000: { cmd: "enter", room: "TPCAHallway", label: "Unceded territory", point: [10, 19], enterPoint: [114, 60], obstacle: false },
         },
 		things: {
             //sprite spreadsheets only 1 row ok?
