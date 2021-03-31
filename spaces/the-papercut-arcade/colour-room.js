da0e3ce9a167bcfad72fbbeb5ca3dc04498716b3
negaspace <thunderblossom@gmail.com>
2021-03-31 01:32:58 -0700

    Animations for TPCAColourRoom
    Animations added and positions adjusted for TPCAColourRoom

--- spaces/the-papercut-arcade/colour-room.js
+++ spaces/the-papercut-arcade/colour-room.js
@@ -12,9 +12,9 @@ module.exports = {
             hff0000: { cmd: "enter", room: "TPCAHallway2", label: "out", point: [65, 94], enterPoint: [32, 16], obstacle: false },
         },
 		things: {
-			instrument: {file: "colourRoom-Music.png", position: [96, 15], obstacle: true, lightEmissionColour: "#e8d42e" },
-			voice: {file: "colourRoom-Voice.png", position: [20, 73], frames: 3, frameDelay: 11, obstacle: true, lightEmissionColour: "#e82e3a"},
-			blender: {file: "colourRoom-Blender.png", position: [95, 58], obstacle: true, lightEmissionColour: "#73dff4"},
+			instrument: {file: "colourRoom-Music.png", position: [75, 15], frames: 4, frameDelay: 8, obstacle: true, lightEmissionColour: "#e8d42e" },
+			voice: {file: "colourRoom-Voice.png", position: [30, 70], frames: 3, frameDelay: 11, obstacle: true, lightEmissionColour: "#e82e3a"},
+			blender: {file: "colourRoom-Blender.png", position: [90, 58], frames: 5, obstacle: true, lightEmissionColour: "#73dff4"},
 			Footstep: {file: "colourRoom-Footstep.png", position: [21, 21], frames: 3, frameDelay: 10, obstacle: true, lightEmissionColour: "#ac73f4" },
 		},
     },
